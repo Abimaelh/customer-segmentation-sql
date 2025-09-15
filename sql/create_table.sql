8e8d77a719db2e6cd0ffbbcf937b4cb7f750aa19
abimaelh <abimaelh@uw.edu>
2025-09-15 13:59:11 -0700

    Track SQL scripts for table creation

---
+++ sql/create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales (
+    InvoiceNo VARCHAR(20),
+    StockCode VARCHAR(20),
+    Description TEXT,
+    Quantity INT,
+    InvoiceDate TIMESTAMP,
+    UnitPrice NUMERIC,
+    CustomerID VARCHAR(20),
+    Country VARCHAR(50)
+);
