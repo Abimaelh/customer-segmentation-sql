80ffccf486c53006672a967d879d0f7aa143b7f1
abimaelh <abimaelh@uw.edu>
2025-09-17 16:03:09 -0700

    Add top 10 analysis script

--- scripts/rfm_analysis.py
+++ scripts/rfm_analysis.py
@@ -1,1 +1,24 @@
-# work-in-progress
+import psycopg2
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+conn = psycopg2.connect(
+    dbname = "ecommerce",
+    user = "abimael",
+    host="localhost",
+    port = "5432"
+)
+
+query = """
+SELECT
+    CustomerID,
+    SUM(UnitPrice * Quantity) AS monetary
+FROM sales_clean
+GROUP BY CustomerID
+ORDER BY monetary DESC
+LIMIT 10;
+"""
+
+df = pd.read_sql(query, conn)
+conn.close()
