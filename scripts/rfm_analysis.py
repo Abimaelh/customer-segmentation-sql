c74bdd4abcbe96fd00146be314f1c75ed3dd1259
abimaelh <abimaelh@uw.edu>
2025-09-26 16:25:34 -0700

    Update RFM query

--- scripts/rfm_analysis.py
+++ scripts/rfm_analysis.py
@@ -13,7 +13,7 @@ conn = psycopg2.connect(
 query = """
 SELECT
     CustomerID,
-    MAX(InvoiceDate) AS last_purchase,
+    DATE_PART('day', CURRENT_DATE - MAX(InvoiceDate)) AS recency,
     COUNT(*) AS frequency,
     SUM(UnitPrice * Quantity) AS total_spend
 FROM sales_clean
